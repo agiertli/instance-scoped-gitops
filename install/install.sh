5cadadfc3d5b0f841d62d06e29e82e525ebb6064
Anton Giertli <agiertli@redhat.com>
2024-06-10 20:46:32 +0200

    refactoring

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,4 @@
-TENANT1=tenant1
-TENANT2=tenant2
+GITOPS_NAMESPACE=openshift-gitops
 
 while getopts "p:" opt; do
   case $opt in
@@ -18,44 +17,19 @@ while getopts "p:" opt; do
 done
 
 echo "Creating Subscription: argocd"
-oc apply -f argocd-subs.yaml
-oc apply -f tenant1-namespace.yaml
-oc apply -f tenant2-namespace.yaml
+oc apply -f cluster-scoped/argocd-subs.yaml
 
-echo "Waiting for operator to be installed..."
-sleep 30
-oc apply -f tenant1-argocd.yaml
-oc apply -f tenant2-argocd.yaml
 
-echo "Patching... ${TENANT1}"
-
-while [[ argoSecret=$(oc -n ${TENANT1} get secret ${TENANT1}-gitops-cluster 2>&1 > /dev/null) == *"Error"* ]]
+while [[ argoSecret=$(oc -n $GITOPS_NAMESPACE get secret $GITOPS_NAMESPACE-cluster 2>&1 > /dev/null) == *"Error"* ]]
 do
 echo "Waiting until ArgoCD CR is ready..."
 sleep 5
 done
-oc -n $TENANT1 patch secret $TENANT1-gitops-cluster --patch "{\"stringData\": {\"admin.password\": \"$ARGOCD_ADMIN_PASSWORD\"}}"
-oc -n $TENANT1 patch secret argocd-secret --patch "{\"data\": {\"admin.password\": null}}"
-
-echo "Patching... ${TENANT2}"
-
-while [[ argoSecret=$(oc -n ${TENANT2} get secret ${TENANT2}-gitops-cluster 2>&1 > /dev/null) == *"Error"* ]]
-do
-echo "Waiting until ArgoCD CR is ready..."
-sleep 5
-done
-oc -n $TENANT2 patch secret $TENANT2-gitops-cluster --patch "{\"stringData\": {\"admin.password\": \"$ARGOCD_ADMIN_PASSWORD\"}}"
-oc -n $TENANT2 patch secret argocd-secret --patch "{\"data\": {\"admin.password\": null}}"
-
-oc apply -f tenant1-project.yaml
-oc apply -f tenant2-project.yaml
+oc -n $GITOPS_NAMESPACE patch secret $GITOPS_NAMESPACE-cluster --patch "{\"stringData\": {\"admin.password\": \"$ARGOCD_ADMIN_PASSWORD\"}}"
+oc -n $GITOPS_NAMESPACE patch secret argocd-secret --patch "{\"data\": {\"admin.password\": null}}"
 
 
-oc patch configmaps argocd-rbac-cm  -p '{"data":{"policy.default":" "}}' -n $TENANT1
-oc patch configmaps argocd-rbac-cm  -p '{"data":{"policy.default":" "}}' -n $TENANT2
 
-oc apply -f tenant1-app-of-apps.yaml
-oc apply -f tenant2-app-of-apps.yaml
-
+oc apply -f cluster-scoped/application-set.yaml
 
 echo "Installation complete!" 
